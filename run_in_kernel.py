bbb39b330715b03e0ff364e70256f2a034b5e84e
jakubczakon <czakon.jakub@gmail.com>
2019-07-20 13:15:09 +0200

    updated running in kernel

--- run_in_kernel.py
+++ run_in_kernel.py
@@ -1,16 +1,16 @@
+import os
 import sys
 import subprocess
-
 subprocess.call(['pip', 'install', 'neptune-client', 'neptune-contrib'])
 
-REPO_PATH = "../input/ieee-repo/repository/neptune-ml-kaggle-ieee-fraud-detection-fbedab9"
+REPO_PATH = "../input/ieee-repo/repository/neptune-ml-kaggle-ieee-fraud-detection-7aa0453"
 sys.path.append(REPO_PATH)
 
 os.environ["CONFIG_PATH"] = os.path.join(REPO_PATH, 'kaggle_config.yaml')
 os.environ["NEPTUNE_API_TOKEN"] = "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vdWkubmVwdHVuZS5tbCIsImFwaV9rZXkiOiJiNzA2YmM4Zi03NmY5LTRjMmUtOTM5ZC00YmEwMzZmOTMyZTQifQ=="
 
-# Feature extraction
-# subprocess.call(['python', os.path.join(REPO_PATH, 'src', 'features', 'feature_extraction_v0.py')])
+# # Feature extraction
+# # subprocess.call(['python', os.path.join(REPO_PATH, 'src', 'features', 'feature_extraction_v0.py')])
 
 # Model training
 subprocess.call(['python', os.path.join(REPO_PATH, 'src', 'models', 'train_lgbm_holdout.py')])
