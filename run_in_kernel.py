983cd496d0d0537f52fab7ee643b249a0c63ef0a
jakubczakon <czakon.jakub@gmail.com>
2019-07-21 14:20:15 +0200

    updated instrutions

--- run_in_kernel.py
+++ run_in_kernel.py
@@ -4,7 +4,7 @@ import subprocess
 
 REPO_LOCATION = 'https://github.com/neptune-ml/kaggle-ieee-fraud-detection.git'
 REPO_NAME = 'kaggle-ieee-fraud-detection'
-REPO_BRANCH = 'master'
+REPO_BRANCH = 'dev'
 PACKAGES = ['neptune-client', 'neptune-contrib']
 NEPTUNE_API_TOKEN = 'eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vdWkubmVwdHVuZS5tbCIsImFwaV9rZXkiOiJiNzA2YmM4Zi03NmY5LTRjMmUtOTM5ZC00YmEwMzZmOTMyZTQifQ=='
 CONFIG_NAME = 'config_kaggle.yml'
@@ -24,9 +24,9 @@ os.environ["NEPTUNE_API_TOKEN"] = NEPTUNE_API_TOKEN
 sys.path.append(REPO_NAME)
 
 # Feature extraction
-from src.features import feature_extraction_v0
-print('extracting features')
-feature_extraction_v0.main()
+# from src.features import feature_extraction_v0
+# print('extracting features')
+# feature_extraction_v0.main()
 
 # Model training
 from src.models import train_lgbm_holdout
