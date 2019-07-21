ea5fed2bb016644ebae4b6068617b0a52e6eb5b3
jakubczakon <czakon.jakub@gmail.com>
2019-07-21 13:00:14 +0200

    updated configs and instructions

--- run_in_kernel.py
+++ run_in_kernel.py
@@ -1,16 +1,37 @@
 import os
 import sys
 import subprocess
-subprocess.call(['pip', 'install', 'neptune-client', 'neptune-contrib'])
 
-REPO_PATH = "../input/ieee-repo/repository/neptune-ml-kaggle-ieee-fraud-detection-7aa0453"
-sys.path.append(REPO_PATH)
+REPO_LOCATION = 'https://github.com/neptune-ml/kaggle-ieee-fraud-detection.git'
+REPO_NAME = 'kaggle-ieee-fraud-detection'
+PACKAGES = ['neptune-client', 'neptune-contrib']
+NEPTUNE_API_TOKEN = 'eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vdWkubmVwdHVuZS5tbCIsImFwaV9rZXkiOiJiNzA2YmM4Zi03NmY5LTRjMmUtOTM5ZC00YmEwMzZmOTMyZTQifQ=='
+CONFIG_NAME = 'kaggle_config.yml'
 
-os.environ["CONFIG_PATH"] = os.path.join(REPO_PATH, 'kaggle_config.yaml')
-os.environ["NEPTUNE_API_TOKEN"] = "eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vdWkubmVwdHVuZS5tbCIsImFwaV9rZXkiOiJiNzA2YmM4Zi03NmY5LTRjMmUtOTM5ZC00YmEwMzZmOTMyZTQifQ=="
+# Clone the repository
+print('cloning the repository')
+subprocess.call(['git', 'clone', REPO_LOCATION])
 
-# # Feature extraction
-# # subprocess.call(['python', os.path.join(REPO_PATH, 'src', 'features', 'feature_extraction_v0.py')])
+print('adding repository src to path')
+sys.path.append(REPO_NAME)
+
+# Install packages
+print('installing packages')
+subprocess.call(['pip', 'install'] + PACKAGES)
+
+print('setting environment variables')
+os.environ["CONFIG_PATH"] = os.path.join(REPO_NAME, CONFIG_NAME)
+os.environ["NEPTUNE_API_TOKEN"] = NEPTUNE_API_TOKEN
+
+# Feature extraction
+# print('extracting features')
+# from src.features import feature_extraction_v0
+# feature_extraction_v0.main()
 
 # Model training
-subprocess.call(['python', os.path.join(REPO_PATH, 'src', 'models', 'train_lgbm_holdout.py')])
+from src.models import train_lgbm_holdout
+print('training model')
+train_lgbm_holdout.main()
+
+
+
